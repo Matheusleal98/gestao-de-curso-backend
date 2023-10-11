d210543a8e297465377ff583ac00501421bea5d2
leal <matheusskeal98@gmail.com>
2023-10-11 15:47:59 -0300

    Ajuste na classe Aluno.

--- src/main/kotlin/com/example/studentcode/entity/Aluno.kt
+++ src/main/kotlin/com/example/studentcode/entity/Aluno.kt
@@ -4,13 +4,16 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
+@Table(name = "tb_aluno")
 @Entity
-data class Aluno(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long?,
-    val nome: String,
-    val email: String,
-    val dt_nascimento: String,
-    val ra: String
-)
+class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = 0
+    var nome: String? = null
+    var email: String? = null
+    var dt_nascimento: String? = null
+    var ra: String? = null
+}
