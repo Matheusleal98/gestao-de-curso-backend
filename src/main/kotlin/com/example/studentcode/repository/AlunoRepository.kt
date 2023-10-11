e75ecdfb149fb5fa31ad595084d7a6eeffc4b175
leal <matheusskeal98@gmail.com>
2023-10-11 10:27:25 -0300

    Criando as classes repository.

---
+++ src/main/kotlin/com/example/studentcode/repository/AlunoRepository.kt
@@ -0,0 +1,9 @@
+package com.example.studentcode.repository
+
+import com.example.studentcode.entity.Aluno
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+open interface AlunoRepository : JpaRepository<Aluno, Long?> {
+}
