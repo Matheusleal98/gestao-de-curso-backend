ffe10caeea560fe6b031cf832206c696c4e58ab2
leal <matheusskeal98@gmail.com>
2023-10-11 09:54:51 -0300

    Iniciando o projeto

---
+++ src/main/kotlin/com/example/studentcode/StudentcodeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.studentcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudentcodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudentcodeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/studentcode/StudentcodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.studentcode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudentcodeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
