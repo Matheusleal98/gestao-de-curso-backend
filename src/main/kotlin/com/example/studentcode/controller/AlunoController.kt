60bb3d3c27a79d81981b63811e68c3a91dd2e08c
leal <matheusskeal98@gmail.com>
2023-10-11 17:42:02 -0300

    configurando application e implementando na controller aluno.

--- src/main/kotlin/com/example/studentcode/controller/AlunoController.kt
+++ src/main/kotlin/com/example/studentcode/controller/AlunoController.kt
@@ -3,19 +3,18 @@ package com.example.studentcode.controller
 import com.example.studentcode.dto.AlunoDTO
 import com.example.studentcode.entity.Aluno
 import com.example.studentcode.service.AlunoService
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/admin")
 class AlunoController {
 
     private lateinit var alunoService: AlunoService
 
-    @PostMapping
+    @PostMapping("/cadastro-aluno")
     fun criarAluno(@RequestBody dto: AlunoDTO): Aluno {
         return alunoService.criarAluno(dto)
     }
